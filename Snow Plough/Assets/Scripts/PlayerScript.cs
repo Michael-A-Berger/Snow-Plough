0d4df7df801c7284889f3b988f3a67a95a346988
Michael Berger <michael.anthony.berger@gmail.com>
2020-01-13 08:34:06 -0500

    Added debug environment + Restricted player movement to developer-defined Play Field area

--- Snow Plough/Assets/Scripts/PlayerScript.cs
+++ Snow Plough/Assets/Scripts/PlayerScript.cs
@@ -7,6 +7,7 @@ public class PlayerScript : MonoBehaviour
     // PUBLIC ATTRIBUTES
     public float baseSpeed = 6f;
     public float dashSpeed = 12f;
+    public Rect playField = new Rect(-5f, -5f, 10f, 10f);
     public bool debug = false;
 
     // PRIVATE ATTRIBUTES
@@ -53,6 +54,16 @@ public class PlayerScript : MonoBehaviour
     // LateUpdate()
     private void LateUpdate()
     {
-        
+        // Clamping the player to the play field
+        Vector3 pos = transform.position;
+        if (pos.x < playField.x || pos.x > playField.x + playField.width)
+        {
+            pos.x = Mathf.Clamp(pos.x, playField.x, playField.x + playField.width);
+        }
+        if (pos.z < playField.y || pos.z > playField.y + playField.height)
+        {
+            pos.z = Mathf.Clamp(pos.z, playField.y, playField.y + playField.height);
+        }
+        transform.position = pos;
     }
 }
